576f1765b6add7cbd2f37b910277258d8c51b949
dt-bitcoding <dt.bitcoding@gmail.com>
2024-02-08 16:21:31 +0530

    Changes done

--- NovaBazaar/views.py
+++ NovaBazaar/views.py
@@ -14,26 +14,13 @@ def user(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            
-            # first_name = form.cleaned_data['First_name']
-            # print("Hello Data.....")
-            # email = form.cleaned_data['Email']
-            # password = form.cleaned_data['Password']
-            # confirm_password = form.cleaned_data['Confirm_password']
-               
+
             First_name = request.POST.get('First_name', '')
             Email = request.POST.get('Email', '')
             Password = request.POST.get('Password', '')
             Confirm_password = request.POST.get('Confirm_password', '')
-            
-
-            # user_instance = User(FirstName=first_name, Email=email, Password=password, Confirm_password=confirm_password)
-            # user_instance = User(first_name=FirstName, email=Email, password=Password, confirm_password=Confirm_password)
-           
-
-            # user_instance = User(firstName=first_name, email=email, password=password, confirm_password=confirm_password)
+        
             user_instance = User(FirstName=First_name, Email=Email, Password=Password, confirm_password=Confirm_password)
-            print("Hello Data.....")
             user_instance.save()
             
             return redirect('/success')
@@ -53,4 +40,3 @@ def Userlogin(request):
             return HttpResponse('Invalid Credentials')
         
     return render(request, 'NovaBazaar/login.html')
-
