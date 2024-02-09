73f1d94be213a94d65647bbf4cc3862dfeb4ca5d
dt-bitcoding <dt.bitcoding@gmail.com>
2024-02-09 10:18:09 +0530

    commit changes is done

--- NovaBazaar/forms.py
+++ NovaBazaar/forms.py
@@ -1,12 +1,27 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 
 class MyForm(forms.Form):
     FirstName = forms.CharField(max_length=100)
     Email = forms.EmailField(max_length=100)
-    Password = forms.CharField(max_length=100)
-    Confirm_password = forms.CharField(max_length=100)
+    Password = forms.CharField(widget=forms.PasswordInput)
+    Confirm_password = forms.CharField(widget=forms.PasswordInput)
 
+    def clean(self):
+        
+        cleaned_data = super().clean()
+        password = cleaned_data.get("Password")
+        # print("password ", password)
+        confirm_password = cleaned_data.get("Confirm_password")
+        # print("Confirm Password ", confirm_password)
+        
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+    
+        return cleaned_data
 
 class Form(forms.Form):
-    Email = forms.EmailField(max_length=100)
+    email = forms.EmailField(max_length=100)
+
     Password = forms.CharField(max_length=100)
--- NovaBazaar/views.py
+++ NovaBazaar/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import User
 from .forms import MyForm, Form
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+ 
 
 def success_view(request):
     return render(request, 'NovaBazaar/success.html')
@@ -10,17 +12,20 @@ def success_view(request):
 def signup(request):
     return render(request, 'NovaBazaar/signup.html')
 
+def logout(request):
+    return render(request, 'NovaBazaar/index.html')
+
 def user(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
 
-            Firstname = request.POST.get('Firstname', '')
-            Email = request.POST.get('Email', '')
-            Password = request.POST.get('Password', '')
-            Confirm_password = request.POST.get('Confirm_password', '')
+            firstname = request.POST.get('Firstname', '')
+            email = request.POST.get('Email', '')
+            password = request.POST.get('Password', '')
+            confirmPassword = request.POST.get('Confirm_password', '')
         
-            user_instance = User(FirstName=Firstname, Email=Email, Password=Password, confirm_password=Confirm_password)
+            user_instance = User(FirstName=firstname, Email=email, Password=password, confirm_password=confirmPassword)
             user_instance.save()
             
             return redirect('/success')
@@ -30,24 +35,32 @@ def user(request):
     return render(request, 'NovaBazaar/index.html', {'form': form})
  
 
-def Userlogin(request):
+# def Userlogin(request):
+#     if request.method == 'POST':
+#         form = Form(request.POST)
+#         if form.is_valid():
+#             email = request.POST.get('email', '')
+#             Password = request.POST.get('Password', '')
+            
+#             user_instance = User(Email=email, Password=Password)
+#             user_instance.save()
+#             return redirect('/loginuser')
+#     else:
+#         form = Form()
+#     return render(request, 'NovaBazaar/login.html', {'form': form})
     
+def Userlogin(request):
     if request.method == 'POST':
-        form = Form(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-
-            Email = request.POST.get('Email', '')
+            email = request.POST.get('email', '')
             Password = request.POST.get('Password', '')
-            
-            user_instance = User(Email=Email, Password=Password)
-            user_instance.save()
-            
-            return redirect('/loginuser')
-    else:
-        form = Form()
+            user = authenticate(request, Email=email, Password=Password)
 
+            if user is not None:
+                login(request, user)
+                return redirect('index.html')  # Redirect to the home page after successful login
+    else:
+        form = AuthenticationForm()
     return render(request, 'NovaBazaar/login.html', {'form': form})
 
-
-def logout(request):
-    return render(request, 'NovaBazaar/index.html')
