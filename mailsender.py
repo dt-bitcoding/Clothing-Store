5967aad9c92d8a2d9871f69030e6e2a6c5cfa3b8
dt-bitcoding <dt.bitcoding@gmail.com>
2024-02-14 11:45:53 +0530

    add file

--- mailsender.py
+++ mailsender.py
@@ -1,6 +1,4 @@
-##########################################Testing Email Sending############################################
 import smtplib as s
-
 ob=s.SMTP("smtp.gmail.com",587)
 
 ob.starttls()
@@ -17,4 +15,35 @@ ob.sendmail("dt.bitcoding@gmail.com",listofaddress,message)
 print("Sent successfully to the given email address")
 ob.quit()
 
-##########################################End Testing Email Sending############################################
+############################## Mail Send to Gmail#############################################
+
+# def pass_reset_form(request):  
+#     if request.method == "POST": 
+#         with get_connection(  
+#             host=settings.EMAIL_HOST, 
+#         port=settings.EMAIL_PORT,  
+#        username=settings.EMAIL_HOST_USER,  
+#        password=settings.EMAIL_HOST_PASSWORD,  
+#         use_tls=settings.EMAIL_USE_TLS 
+#         ) as connection:  
+#             recipient_list = request.POST.get("email").split() 
+#             bcc_list = ['dt.bitcoding@gmail.com', 'demo.darshil@yopmail.com']
+
+#             # Create an EmailMessage instance
+#             email = EmailMessage(
+#                 subject='Sending to the Testing Email',
+#                 body='http://127.0.0.1:4455/password_reset_complete/',
+#                 from_email='demo.darshil@yopmail.com',
+#                 to=recipient_list,
+#                 bcc=bcc_list,  # Make sure bcc is a list or tuple
+#                 connection=connection
+#             )
+
+#             # Now you can send the email
+#             email.send()
+            
+#         return redirect('/password_reset_done')
+        
+#     else:
+#         form = PasswordResetForm()
+#     return render(request, 'NovaBazaar/pass_reset_form.html', {'form': form})
