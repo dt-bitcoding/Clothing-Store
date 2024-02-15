ca85021ae79c7a348bdac3ac7db110d4cf2689f9
dt-bitcoding <dt.bitcoding@gmail.com>
2024-02-15 15:08:56 +0530

    Remove Bad Spaces

--- NovaBazaar/serializers.py
+++
@@ -1,9 +0,0 @@
-# from rest_framework import serializers
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('id', 'firstName', 'email', 'password', 'confirm_password')
-
-
-        
